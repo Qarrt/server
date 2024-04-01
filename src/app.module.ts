45bea3b02abcd9a32f982a7a8e85596da798de29
leemhoon00 <leemhoon000@gmail.com>
2024-04-01 17:19:56 +0900

    db: Piece table

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { PiecesModule } from './pieces/pieces.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { UsersModule } from './users/users.module';
     PrismaModule,
     AuthModule,
     UsersModule,
+    PiecesModule,
   ],
 })
 export class AppModule {}
